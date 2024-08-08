95238feb93cd5bc4b5cdbec673fd5bb17b6b06bd
wpsimon09 <simon.potocnak03@gmail.com>
2024-08-08 19:44:28 +0200

    recorded the draw command to the command buffer

--- Includes/HelloTriangle.cpp
+++ Includes/HelloTriangle.cpp
@@ -596,6 +596,32 @@ void HelloTriangle::RecordCommandBuffer(VkCommandBuffer commandBuffer, uint32_t 
 
     vkCmdBeginRenderPass(commandBuffer, &renderPassInfo, VK_SUBPASS_CONTENTS_INLINE);
 
+    vkCmdBindPipeline(commandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS, m_graphicsPipeline);
+
+    VkViewport viewport{};
+    viewport.x = 0.0f;
+    viewport.y = 0.0f;
+
+    viewport.width = static_cast<float>(m_swapChainExtent.width);
+    viewport.height = static_cast<float>(m_swapChainExtent.height);
+
+    viewport.minDepth = 0.0f;
+    viewport.maxDepth = 1.0f;
+    vkCmdSetViewport(commandBuffer, 0, 1, &viewport);
+
+    VkRect2D scissors{};
+    scissors.offset = {0,0};
+    scissors.extent = m_swapChainExtent;
+    vkCmdSetScissor(commandBuffer, 0, 1, &scissors);
+
+    vkCmdDraw(commandBuffer, 3,1,0,0);
+
+    vkCmdEndRenderPass(commandBuffer);
+
+    if(vkEndCommandBuffer(commandBuffer) != VK_SUCCESS) {
+        throw std::runtime_error("Failed to record command buffer");
+    }
+
 
 }
 
