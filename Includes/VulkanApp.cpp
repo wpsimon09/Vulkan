bf193fa6cbc6d446fb61e2a9a0593584501c95af
wpsimon09 <simon.potocnak03@gmail.com>
2024-08-24 11:36:31 +0200

    added new layout to include image and sampler to the descriptor layout

--- Includes/VulkanApp.cpp
+++ Includes/VulkanApp.cpp
@@ -418,15 +418,23 @@ void VulkanApp::CreateRenderPass() {
 void VulkanApp::CreateDescriptorSetLayout() {
     VkDescriptorSetLayoutBinding uboLayoutBinding{};
     uboLayoutBinding.binding = 0;
-
     uboLayoutBinding.descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
     uboLayoutBinding.descriptorCount = 1;
     uboLayoutBinding.stageFlags = VK_SHADER_STAGE_VERTEX_BIT;
     uboLayoutBinding.pImmutableSamplers = nullptr;
 
+    VkDescriptorSetLayoutBinding imageSamplerLayoutBinding{};
+    imageSamplerLayoutBinding.binding = 1;
+    imageSamplerLayoutBinding.descriptorCount = 1;
+    imageSamplerLayoutBinding.descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
+    imageSamplerLayoutBinding.pImmutableSamplers = nullptr;
+    imageSamplerLayoutBinding.stageFlags = VK_SHADER_STAGE_FRAGMENT_BIT;
+
+    std::array<VkDescriptorSetLayoutBinding,2> bindings = {uboLayoutBinding, imageSamplerLayoutBinding};
+
     VkDescriptorSetLayoutCreateInfo layoutInfo{.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO};
-    layoutInfo.bindingCount = 1;
-    layoutInfo.pBindings = &uboLayoutBinding;
+    layoutInfo.bindingCount = static_cast<uint32_t>(bindings.size());
+    layoutInfo.pBindings = bindings.data();
     layoutInfo.pNext = nullptr;
 
     if(vkCreateDescriptorSetLayout(m_device, &layoutInfo, nullptr, &m_descriptorSetLayout) != VK_SUCCESS) {
@@ -447,7 +455,6 @@ void VulkanApp::CreateDescriptorPool() {
     if(vkCreateDescriptorPool(m_device, &poolInfo, nullptr, &m_descriptorPool) != VK_SUCCESS) {
         throw std::runtime_error("Failed to create descriptor pool");
     }
-
 }
 
 void VulkanApp::CreateDescriptorSet() {
