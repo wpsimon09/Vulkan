c160ddb9196b7b7a08026b767bc606fc05ce9b2c
wpsimon09 <simon.potocnak03@gmail.com>
2024-08-31 10:19:34 +0200

    retrieved maximum available samples

--- Includes/VulkanApp.cpp
+++ Includes/VulkanApp.cpp
@@ -279,6 +279,7 @@ void VulkanApp::PickPhysicalDevice() {
         if (isDeviceSuitable(device, m_sruface)) {
             m_physicalDevice = device;
             VkPhysicalDeviceProperties props;
+            m_msaaSamples = GetMaxUsableSampleCount();
             vkGetPhysicalDeviceProperties(device, &props);
             std::cout << "Using:" << props.deviceName << std::endl;
             break;
@@ -1323,6 +1324,15 @@ VkSampleCountFlagBits VulkanApp::GetMaxUsableSampleCount() {
     VkSamplerCreateFlags counts = physicalDeviceProperties.limits.framebufferColorSampleCounts
     &
     physicalDeviceProperties.limits.framebufferDepthSampleCounts;
+
+    if(counts & VK_SAMPLE_COUNT_64_BIT){return VK_SAMPLE_COUNT_64_BIT;}
+    if(counts & VK_SAMPLE_COUNT_32_BIT){return VK_SAMPLE_COUNT_32_BIT;}
+    if(counts & VK_SAMPLE_COUNT_16_BIT){return VK_SAMPLE_COUNT_16_BIT;}
+    if(counts & VK_SAMPLE_COUNT_8_BIT){return VK_SAMPLE_COUNT_8_BIT;}
+    if(counts & VK_SAMPLE_COUNT_4_BIT){return VK_SAMPLE_COUNT_4_BIT;}
+    if(counts & VK_SAMPLE_COUNT_2_BIT){return VK_SAMPLE_COUNT_2_BIT;}
+
+    return VK_SAMPLE_COUNT_1_BIT;
 }
 
 std::vector<const char *> VulkanApp::GetRequiredExtentions() {
