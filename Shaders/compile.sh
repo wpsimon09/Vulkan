4a61f09c023b2f3e083cb717a17bc2a12b63e1f4
wpsimon09 <simon.potocnak03@gmail.com>
2024-09-07 09:53:01 +0200

    removed assimp from cmake

--- Shaders/compile.sh
+++ Shaders/compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VULKAN_SDK_PATH="/home/wpsimon09/vulkan-sdk/1.3.290.0/x86_64/bin/glslc"
+VULKAN_SDK_PATH="/home/wpsimon09/SDKs/vuklan-sdk/x86_64/bin/glslc"
 
 if [[ ! -x "$VULKAN_SDK_PATH" ]]; then
     echo "Error: Vulkan SDK compiler not found at $VULKAN_SDK_PATH"
